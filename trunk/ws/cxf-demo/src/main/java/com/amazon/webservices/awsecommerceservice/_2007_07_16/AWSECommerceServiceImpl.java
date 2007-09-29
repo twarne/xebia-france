2aa7c8b2054f9c247ba425977ca2b7ef7b17f6b5
cyrille.leclerc@gmail.com <cyrille.leclerc@gmail.com@63a6d7ea-0d3b-0410-ae18-9770a892219d>
2007-09-29 11:41:00 +0000

    cxf-demo project initial commit

---
+++ trunk/ws/cxf-demo/src/main/java/com/amazon/webservices/awsecommerceservice/_2007_07_16/AWSECommerceServiceImpl.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2007 Xebia and the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.amazon.webservices.awsecommerceservice._2007_07_16;
+
+import javax.jws.HandlerChain;
+
+/**
+ * @author <a href="mailto:cyrille.leclerc@pobox.com">Cyrille Le Clerc</a>
+ */
+@HandlerChain(file = "AWSECommerceServiceImplCandlerChain.xml")
+public class AWSECommerceServiceImpl implements AWSECommerceServicePortType {
+
+    public BrowseNodeLookupResponse browseNodeLookup(BrowseNodeLookup body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public CartAddResponse cartAdd(CartAdd body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public CartClearResponse cartClear(CartClear body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public CartCreateResponse cartCreate(CartCreate body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public CartGetResponse cartGet(CartGet body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public CartModifyResponse cartModify(CartModify body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public CustomerContentLookupResponse customerContentLookup(CustomerContentLookup body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public CustomerContentSearchResponse customerContentSearch(CustomerContentSearch body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public HelpResponse help(Help body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public ItemLookupResponse itemLookup(ItemLookup body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public ItemSearchResponse itemSearch(ItemSearch body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public ListLookupResponse listLookup(ListLookup body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public ListSearchResponse listSearch(ListSearch body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public MultiOperationResponse multiOperation(MultiOperation body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public SellerListingLookupResponse sellerListingLookup(SellerListingLookup body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public SellerListingSearchResponse sellerListingSearch(SellerListingSearch body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public SellerLookupResponse sellerLookup(SellerLookup body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public SimilarityLookupResponse similarityLookup(SimilarityLookup body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public TagLookupResponse tagLookup(TagLookup body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public TransactionLookupResponse transactionLookup(TransactionLookup body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
