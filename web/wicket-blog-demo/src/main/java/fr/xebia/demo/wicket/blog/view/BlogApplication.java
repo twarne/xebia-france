dd5bddd7025029426600f0b86e708f920ecf2422
manuel.eveno <manuel.eveno@63a6d7ea-0d3b-0410-ae18-9770a892219d>
2008-03-05 14:56:33 +0000

    Suppression de LocalizerUtils Correction de quelques bugs

--- web/wicket-blog-demo/src/main/java/fr/xebia/demo/wicket/blog/view/BlogApplication.java
+++ web/wicket-blog-demo/src/main/java/fr/xebia/demo/wicket/blog/view/BlogApplication.java
@@ -33,6 +33,7 @@ import fr.xebia.demo.wicket.blog.view.admin.post.PostListPage;
 import fr.xebia.demo.wicket.blog.view.security.AnnotationAuthorizationStrategy;
 import fr.xebia.demo.wicket.blog.view.security.UnauthorizedComponentInstantiationListener;
 
+// TODO Corriger les label des boutons submit
 public class BlogApplication extends SpringWebApplication {
 
     public BlogApplication() {
---
+++ web/wicket-blog-demo/src/test/java/fr/xebia/demo/wicket/blog/view/BasePageTest.java
@@ -0,0 +1,46 @@
+package fr.xebia.demo.wicket.blog.view;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.util.tester.WicketTester;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BasePageTest /*extends WicketPageTest*/ {
+
+    private static WicketTester tester;
+
+    @BeforeClass
+    public static void setUpClass() {
+        tester = new WicketTester();
+    }
+    
+    @Test
+    public void testBasePage() {
+        String[] expectedInfoMessages = { "infoMessage" };
+        String[] expectedErrorMessages = { "errorMessage", "exceptionMessage" };
+        PageParameters pageParameters = new PageParameters();
+        pageParameters.put(BasePage.PARAM_MESSAGE_KEY, expectedInfoMessages[0]);
+        pageParameters.put(BasePage.PARAM_ERRORMESSAGE_KEY, expectedErrorMessages[0]);
+        pageParameters.put(BasePage.PARAM_EXCEPTION_KEY, new Exception(expectedErrorMessages[1]));
+        tester.startPage(TestPage.class, pageParameters);
+        tester.assertRenderedPage(TestPage.class);
+        tester.assertInfoMessages(expectedInfoMessages);
+        tester.assertErrorMessages(expectedErrorMessages);
+    }
+
+    public static class TestPage extends BasePage {
+        private static final long serialVersionUID = 1L;
+
+        public TestPage(PageParameters pageParameters) {
+            super(pageParameters);
+        }
+
+        @Override
+        protected List<MenuItem> getMenuItems() {
+            return new LinkedList<MenuItem>();
+        }
+    }
+}
