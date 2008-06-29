99ba1b8b6c151e632e15ac331773eb3696f0e2bb
njozwiak <njozwiak@63a6d7ea-0d3b-0410-ae18-9770a892219d>
2008-06-29 15:28:44 +0000

    add projects

---
+++ mobilePhoneFlex/mobilePhoneShop-web/src/main/java/fr/xebia/xke/domain/MobilePhone.java
@@ -0,0 +1,59 @@
+package fr.xebia.xke.domain;
+
+public class MobilePhone {
+
+	private long id;
+	private String name;
+	private String image;
+	private String description;
+	private Integer stock;
+	private Double price;
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Integer getStock() {
+		return stock;
+	}
+
+	public void setStock(Integer stock) {
+		this.stock = stock;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
