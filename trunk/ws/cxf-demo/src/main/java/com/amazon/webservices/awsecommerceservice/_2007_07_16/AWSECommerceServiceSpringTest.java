3c9929c47c77a24a0545570fdc3da456d1135698
cyrille.leclerc@gmail.com <cyrille.leclerc@gmail.com@63a6d7ea-0d3b-0410-ae18-9770a892219d>
2008-07-09 13:31:02 +0000

    update

--- trunk/ws/cxf-demo/src/main/java/com/amazon/webservices/awsecommerceservice/_2007_07_16/AWSECommerceServiceSpringTest.java
+++ trunk/ws/cxf-demo/src/main/java/com/amazon/webservices/awsecommerceservice/_2007_07_16/AWSECommerceServiceSpringTest.java
@@ -15,32 +15,50 @@
  */
 package com.amazon.webservices.awsecommerceservice._2007_07_16;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
 import java.util.Map;
 
 import javax.xml.ws.BindingProvider;
 
-import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @author <a href="mailto:cyrille.leclerc@pobox.com">Cyrille Le Clerc</a>
  */
-public class AWSECommerceServiceSpringTest extends AbstractDependencyInjectionSpringContextTests {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
+public class AWSECommerceServiceSpringTest {
 
-    protected AWSECommerceServicePortType awseCommerceServicePortType;
-
-    protected String[] getConfigLocations() {
-        return new String[]{"classpath:com/amazon/webservices/awsecommerceservice/_2007_07_16/beans.xml"};
+    static {
+        System.err.println("set validation mode");
+        System.setProperty("org.apache.spring.validation.mode", "VALIDATION_NONE");
     }
+
+    @Autowired
+    AWSECommerceServicePortType awseCommerceServicePortType;
+
+    @Autowired
+    ApplicationContext applicationContext;
 
     public void setAwseCommerceServicePortType(AWSECommerceServicePortType awseCommerceServicePortType) {
         this.awseCommerceServicePortType = awseCommerceServicePortType;
     }
 
+    @Test
     public void testItemSearch() throws Exception {
 
         assertNotNull(this.awseCommerceServicePortType);
-        for (String beanName : getApplicationContext().getBeanDefinitionNames()) {
-            System.out.println(beanName);
+
+        for (String beanName : applicationContext.getBeanDefinitionNames()) {
+            System.out.println("bean : " + beanName);
         }
         Map<String, Object> requestContext = ((BindingProvider) awseCommerceServicePortType).getRequestContext();
 
