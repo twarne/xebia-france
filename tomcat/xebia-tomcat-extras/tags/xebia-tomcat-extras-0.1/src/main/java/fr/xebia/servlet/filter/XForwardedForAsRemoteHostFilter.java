42799a5395525d7b91c697ea3a531d8b7416d3bc
cyrille.leclerc <cyrille.leclerc@63a6d7ea-0d3b-0410-ae18-9770a892219d>
2009-05-04 02:03:58 +0000

    fix bug in http headers x-original-... values

--- tomcat/xebia-tomcat-extras/tags/xebia-tomcat-extras-0.1/src/main/java/fr/xebia/servlet/filter/XForwardedForAsRemoteHostFilter.java
+++ tomcat/xebia-tomcat-extras/tags/xebia-tomcat-extras-0.1/src/main/java/fr/xebia/servlet/filter/XForwardedForAsRemoteHostFilter.java
@@ -28,9 +28,6 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.log4j.Logger;
 
 /**
  * @author <a href="mailto:cyrille.leclerc@pobox.com">Cyrille Le Clerc</a>
@@ -66,8 +63,10 @@ public class XForwardedForAsRemoteHostFilter implements Filter {
         
         @Override
         public String getHeader(String name) {
-            if (X_ORIGINAL_REMOTE_ADDR_HEADER_NAME.equalsIgnoreCase(name) || X_ORIGINAL_REMOTE_HOST_HEADER_NAME.equalsIgnoreCase(name)) {
-                return xForwardedFor;
+            if (X_ORIGINAL_REMOTE_ADDR_HEADER_NAME.equalsIgnoreCase(name)) {
+                return originalRemoteAddr;
+            } else if (X_ORIGINAL_REMOTE_HOST_HEADER_NAME.equalsIgnoreCase(name)) {
+                return originalRemoteHost;
             } else {
                 return super.getHeader(name);
             }
@@ -86,8 +85,10 @@ public class XForwardedForAsRemoteHostFilter implements Filter {
         
         @Override
         public Enumeration<?> getHeaders(String name) {
-            if (X_ORIGINAL_REMOTE_ADDR_HEADER_NAME.equalsIgnoreCase(name) || X_ORIGINAL_REMOTE_HOST_HEADER_NAME.equalsIgnoreCase(name)) {
-                return Collections.enumeration(Collections.singletonList(xForwardedFor));
+            if (X_ORIGINAL_REMOTE_ADDR_HEADER_NAME.equalsIgnoreCase(name)) {
+                return Collections.enumeration(Collections.singletonList(originalRemoteAddr));
+            } else if (X_ORIGINAL_REMOTE_HOST_HEADER_NAME.equalsIgnoreCase(name)) {
+                return Collections.enumeration(Collections.singletonList(originalRemoteHost));
             } else {
                 return super.getHeaders(name);
             }
