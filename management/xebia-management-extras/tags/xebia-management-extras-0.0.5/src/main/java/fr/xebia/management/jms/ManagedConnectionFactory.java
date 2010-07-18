f05a029892d90ecb91bcd8e8be98e911f5703199
cyrille.leclerc <cyrille.leclerc@63a6d7ea-0d3b-0410-ae18-9770a892219d>
2010-07-18 23:41:27 +0000

    [maven-release-plugin]  copy for tag xebia-management-extras-0.0.5

---
+++ management/xebia-management-extras/tags/xebia-management-extras-0.0.5/src/main/java/fr/xebia/management/jms/ManagedConnectionFactory.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2008-2009 Xebia and the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.xebia.management.jms;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+
+/**
+ * 
+ * @author <a href="mailto:cyrille@cyrilleleclerc.com">Cyrille Le Clerc</a>
+ */
+public class ManagedConnectionFactory implements ConnectionFactory, ManagedConnectionFactoryMBean {
+
+    private ConnectionFactory delegate;
+
+    private final JmsStatistics statistics = new JmsStatistics();
+
+    @Override
+    public Connection createConnection() throws JMSException {
+        try {
+            return new ManagedConnection(delegate.createConnection(), statistics);
+        } catch (JMSException e) {
+            statistics.incrementCreateConnectionExceptionCount();
+            throw e;
+        } catch (RuntimeException e) {
+            statistics.incrementCreateConnectionExceptionCount();
+            throw e;
+        } finally {
+            statistics.incrementCreatedConnectionCount();
+        }
+    }
+
+    @Override
+    public Connection createConnection(String userName, String password) throws JMSException {
+        try {
+            return new ManagedConnection(delegate.createConnection(userName, password), statistics);
+        } catch (JMSException e) {
+            statistics.incrementCreateConnectionExceptionCount();
+            throw e;
+        } catch (RuntimeException e) {
+            statistics.incrementCreateConnectionExceptionCount();
+            throw e;
+        } finally {
+            statistics.incrementCreatedConnectionCount();
+        }
+    }
+
+    @Override
+    public int getCreateConnectionExceptionCount() {
+        return statistics.getCreateConnectionExceptionCount();
+    }
+
+    @Override
+    public int getCreatedConnectionCount() {
+        return statistics.getCreatedConnectionCount();
+    }
+
+    @Override
+    public int getCreatedMessageProducerCount() {
+        return statistics.getCreatedMessageProducerCount();
+    }
+
+    @Override
+    public int getCreatedSessionCount() {
+        return statistics.getCreatedSessionCount();
+    }
+
+    @Override
+    public int getCreateMessageProducerExceptionCount() {
+        return statistics.getCreateMessageProducerExceptionCount();
+    }
+
+    @Override
+    public int getCreateSessionExceptionCount() {
+        return statistics.getCreateSessionExceptionCount();
+    }
+
+    public ConnectionFactory getDelegate() {
+        return delegate;
+    }
+
+    @Override
+    public int getReceivedMessageCount() {
+        return statistics.getReceivedMessageCount();
+    }
+
+    @Override
+    public int getReceivedMessageExceptionCount() {
+        return statistics.getReceivedMessageExceptionCount();
+    }
+
+    @Override
+    public long getReceiveMessageDurationInMillis() {
+        return statistics.getReceiveMessageDurationInMillis();
+    }
+
+    @Override
+    public long getSendMessageDurationInMillis() {
+        return statistics.getSendMessageDurationInMillis();
+    }
+
+    @Override
+    public int getSentMessageCount() {
+        return statistics.getSentMessageCount();
+    }
+
+    @Override
+    public int getSentMessageExceptionCount() {
+        return statistics.getSentMessageExceptionCount();
+    }
+
+    public void setDelegate(ConnectionFactory delegate) {
+        this.delegate = delegate;
+    }
+}
