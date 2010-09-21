603919f83d2b04f9ec6de7de50e1b194a5c2eba3
cyrille.leclerc <cyrille.leclerc@63a6d7ea-0d3b-0410-ae18-9770a892219d>
2010-09-21 23:13:29 +0000

    fix shell

--- training/production-ready-application/trunk/src/main/tomcat/bin/setenv.bat
+++ training/production-ready-application/trunk/src/main/tomcat/bin/setenv.bat
@@ -1,13 +1,3 @@
-CATALINA_OPTS_JMX=" \
-   -Dcom.sun.management.jmxremote.port=6969 \
-   -Dcom.sun.management.jmxremote.ssl=false \
-   -Dcom.sun.management.jmxremote.authenticate=false"
-	
+set CATALINA_OPTS_JMX="-Dcom.sun.management.jmxremote.port=6969 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
 
-CATALINA_OPTS="
-   -server $CATALINA_OPTS \
-   -Dorg.apache.juli.logging.diagnostics.dest=STDOUT \
-   $CATALINA_OPTS_JMX \
-   $CATALINA_OPTS_JVM"
-
-export CATALINA_OPTS;
+set CATALINA_OPTS="-server %CATALINA_OPTS% %CATALINA_OPTS_JMX% %CATALINA_OPTS_JVM%"
--- training/production-ready-application/trunk/src/main/tomcat/bin/setenv.sh
+++ training/production-ready-application/trunk/src/main/tomcat/bin/setenv.sh
@@ -1,3 +1,13 @@
-set CATALINA_OPTS_JMX="-Dcom.sun.management.jmxremote.port=6969 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
+CATALINA_OPTS_JMX=" \
+   -Dcom.sun.management.jmxremote.port=6969 \
+   -Dcom.sun.management.jmxremote.ssl=false \
+   -Dcom.sun.management.jmxremote.authenticate=false"
+	
 
-set CATALINA_OPTS="-server %CATALINA_OPTS% %CATALINA_OPTS_JMX% %CATALINA_OPTS_JVM%"
+CATALINA_OPTS="
+   -server $CATALINA_OPTS \
+   -Dorg.apache.juli.logging.diagnostics.dest=STDOUT \
+   $CATALINA_OPTS_JMX \
+   $CATALINA_OPTS_JVM"
+
+export CATALINA_OPTS;
