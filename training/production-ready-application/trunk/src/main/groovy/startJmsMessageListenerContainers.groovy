424e568aa9e1ac262999c8629b927ddb7d4011f0
cyrille.leclerc <cyrille.leclerc@63a6d7ea-0d3b-0410-ae18-9770a892219d>
2011-05-25 11:56:07 +0000

    Add Groovy JMX script

---
+++ training/production-ready-application/trunk/src/main/groovy/startJmsMessageListenerContainers.groovy
@@ -0,0 +1,35 @@
+#!/usr/bin/groovy
+
+import javax.management.ObjectName;
+import javax.management.Query;
+
+import groovy.jmx.builder.JmxBuilder;
+
+/**
+ * 
+ */
+class startJmsMessageListenerContainers {
+
+    static void main(String[] args) {
+        def jmx = new JmxBuilder()
+
+        def jmxClient = jmx.connectorClient (host:"localhost",port:6969)
+        println("Establish JMX Connection to $jmxClient")
+
+        jmxClient.connect()
+
+        def server = jmxClient.getMBeanServerConnection()
+
+        // javax.jms:destination=my-destination,name="fr.xebia.springframework.jms.ManagedDefaultMessageListenerContainer#0",type=MessageListenerContainer,host=localhost,path=/production-ready-application
+        server.queryNames(new ObjectName('javax.jms:type=MessageListenerContainer,*'), null).each { name ->
+            println("Start JMX Message Listener $name")
+            def jmsMessageListenerContainer = new GroovyMBean(server, name)
+            jmsMessageListenerContainer.start()
+            if (jmsMessageListenerContainer.Running == false) {
+                println("FAILURE to start jmsMessageListenerContainer ! $name")
+            }
+        }
+        jmxClient.close()
+        println "JMS message listeners are started. Bye"
+    }
+}
