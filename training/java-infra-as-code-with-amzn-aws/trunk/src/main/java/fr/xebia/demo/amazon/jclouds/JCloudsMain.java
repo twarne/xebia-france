c94acc9ee7a637673e06c6498bb8174d0877432b
dechouxb@gmail.com <dechouxb@gmail.com@63a6d7ea-0d3b-0410-ae18-9770a892219d>
2011-09-07 11:18:54 +0000

    cleaning main class

--- training/java-infra-as-code-with-amzn-aws/trunk/src/main/java/fr/xebia/demo/amazon/jclouds/JCloudsMain.java
+++ training/java-infra-as-code-with-amzn-aws/trunk/src/main/java/fr/xebia/demo/amazon/jclouds/JCloudsMain.java
@@ -8,8 +8,10 @@ import java.util.Set;
 import org.jclouds.compute.ComputeService;
 import org.jclouds.compute.ComputeServiceContext;
 import org.jclouds.compute.ComputeServiceContextFactory;
+import org.jclouds.compute.RunNodesException;
 import org.jclouds.compute.domain.ImageBuilder;
 import org.jclouds.compute.domain.NodeMetadata;
+import org.jclouds.compute.domain.OsFamily;
 import org.jclouds.compute.domain.Template;
 import org.jclouds.ec2.compute.options.EC2TemplateOptions;
 
@@ -19,7 +21,9 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * @see http://code.google.com/p/jclouds/wiki/QuickStartAWS
- * @see https://github.com/jclouds/jclouds-examples/blob/master/ec2-createlamp/src/main/java/org/jclouds/examples/ec2/createlamp/MainApp.java
+ * @see https
+ *      ://github.com/jclouds/jclouds-examples/blob/master/ec2-createlamp/src
+ *      /main/java/org/jclouds/examples/ec2/createlamp/MainApp.java
  */
 public class JCloudsMain {
     private static final String PROVIDER = "aws-ec2";
@@ -38,27 +42,8 @@ public class JCloudsMain {
         ComputeServiceContext context = null;
         try {
             loadProperties();
-
             context = createComputeServiceContext();
-            final ComputeService computeService = context.getComputeService();
-
-            Template template = computeService.templateBuilder().smallest()
-                    .fromImage(new ImageBuilder().id(IMAGE_ID).build())
-                    .locationId(LOCATION_ID).build();
-            template.getOptions().as(EC2TemplateOptions.class).keyPair(keyPair);
-            template.getOptions().blockUntilRunning(true);
-            template.getOptions().tags(Arrays.asList("jclouds-tag"));
-            //template.getOptions().as(EC2TemplateOptions.class).userData(unencodedData)
-            
-            Set<? extends NodeMetadata> nodes = computeService
-                    .createNodesInGroup("jclouds-test", 2, template);
-
-            for (NodeMetadata nodeMetadata : nodes) {
-                System.out.println("created node : " + nodeMetadata.getId());
-            }
-
-            destroyNodesWithReferences(computeService, nodes);
-
+            createAndDestroyPetclinic(context);
         } catch (Exception e) {
             Throwables.propagate(e);
         } finally {
@@ -66,6 +51,20 @@ public class JCloudsMain {
                 context.close();
             }
         }
+    }
+
+    private void createAndDestroyPetclinic(final ComputeServiceContext context)
+            throws RunNodesException {
+        final ComputeService computeService = context.getComputeService();
+        final Template template = createDefaultTemplate(computeService);
+        final Set<? extends NodeMetadata> nodes = computeService
+                .createNodesInGroup("jclouds-test", 2, template);
+
+        for (final NodeMetadata nodeMetadata : nodes) {
+            System.out.println("created node : " + nodeMetadata.getId());
+        }
+
+        destroyNodesWithReferences(computeService, nodes);
     }
 
     /**
@@ -89,12 +88,29 @@ public class JCloudsMain {
     }
 
     /**
+     * TODO do we want to use the same image as for the others demos?
+     * TODO use cloudinit through userdata
+     */
+    private Template createDefaultTemplate(final ComputeService computeService) {
+        Template template = computeService.templateBuilder().smallest()//
+                .osFamily(OsFamily.UBUNTU)
+                // .fromImage(new ImageBuilder().id(IMAGE_ID).build())//
+                .locationId(LOCATION_ID)//
+                .build();
+        template.getOptions().as(EC2TemplateOptions.class).keyPair(keyPair);
+        template.getOptions().blockUntilRunning(true);
+        template.getOptions().tags(Arrays.asList("jclouds-tag"));
+        // template.getOptions().as(EC2TemplateOptions.class).userData(unencodedData);
+        return template;
+    }
+
+    /**
      * Destroy all the provided nodes.
      */
     private void destroyNodesWithReferences(
             final ComputeService computeService,
-            Set<? extends NodeMetadata> nodes) {
-        for (NodeMetadata nodeMetadata : nodes) {
+            final Set<? extends NodeMetadata> nodes) {
+        for (final NodeMetadata nodeMetadata : nodes) {
             computeService.destroyNode(nodeMetadata.getId());
             System.out.println("destroying node : " + nodeMetadata.getId());
         }
