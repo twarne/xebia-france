04a6f6c10673930d9f5821e340e2e454e5c704c9
emmanuel.servent@gmail.com <emmanuel.servent@gmail.com@63a6d7ea-0d3b-0410-ae18-9770a892219d>
2011-09-08 15:34:19 +0000

    Utilisation de JClouds avec une Image Id

---
+++ training/java-infra-as-code-with-amzn-aws/trunk/src/test/java/fr/xebia/demo/amazon/aws/AmazonAwsPetclinicInfrastructureJCloudsEseMakerAnswerTest.java
@@ -0,0 +1,15 @@
+package fr.xebia.demo.amazon.aws;
+
+import org.junit.Test;
+
+
+public class AmazonAwsPetclinicInfrastructureJCloudsEseMakerAnswerTest {
+    
+    @Test
+    public void testCreateImage() {
+
+        AmazonAwsPetclinicInfrastructureJCloudsEseMakerAnswer jClouds = new AmazonAwsPetclinicInfrastructureJCloudsEseMakerAnswer();
+        
+    }
+
+}
